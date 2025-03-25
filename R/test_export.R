97fbdba9949a63508a5a6dc7c134a83f1664a416
Jinhong Cui <cuijinhongqk@gmail.com>
2025-03-25 00:02:35 -0500

    5

---
+++ R/test_export.R
@@ -0,0 +1,9 @@
+#' Say Hello
+#'
+#' This function prints a test message to confirm export is working.
+#'
+#' @return A character string
+#' @export
+say_hello <- function() {
+  print("✅ Exported function is working!")
+}
