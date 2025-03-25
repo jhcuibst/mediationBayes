149eea9e9c3e1d9b8a3c568194b2e9f110b56eb7
Jinhong Cui <cuijinhongqk@gmail.com>
2025-03-24 22:51:42 -0500

    Added example_data dataset with roxygen documentation

---
+++ R/data-docs.R
@@ -0,0 +1,14 @@
+#' Example dataset
+#'
+#' A short description of your dataset.
+#'
+#' @format A data frame with N rows and M variables:
+#' \describe{
+#'   \item{var1}{Description of var1}
+#'   \item{var2}{Description of var2}
+#'   ...
+#' }
+#'
+#' @source Your name or source info
+"example_data"
+
